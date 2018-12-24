12506f8249c28c95cda95fef1bfc925638c155ba
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-24 15:33:07 +0100

    Node js API timer

---
+++ timer.js
@@ -0,0 +1,15 @@
+/*  setInterval(function() {
+  console.log("jamal hassouni");
+}, 1000);
+*/
+
+/* var count = 0;
+console.log(count);
+setInterval(function() {
+  count++;
+  console.log("Jamal Hassouni " + count);
+}, 1000); */
+
+setTimeout(function() {
+  console.log("Hello");
+}, 3000);
