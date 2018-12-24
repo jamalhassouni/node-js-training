1d9f78d14106021a469d81835936022b0bd75367
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-24 15:05:45 +0100

    Node js modules and API OS

--- osys.js
+++ osys.js
@@ -1,14 +1,29 @@
 const os = require("os");
 console.log("====================================");
-console.log(os.freemem()); //  free system memory
-console.log(os.homedir()); // home directory of the current user
-console.log(os.hostname()); // host name
-console.log(os.endianness()); // cpu type
-console.log(os.loadavg()); // method returns an array containing the 1, 5, and 15 minute load averages
-console.log(os.platform()); // paltform
-console.log(os.release()); // kernel release
-console.log(os.tmpdir()); // tmp directory
-console.log(os.totalmem()); // total Memory
-console.log(os.type()); // os type
-console.log(os.uptime()); // os up time
+console.log("free memory : ", os.freemem()); //  free system memory
+console.log("home dir : ", os.homedir()); // home directory of the current user
+console.log("hostname : ", os.hostname()); // host name
+console.log("cpu type : ", os.endianness()); // cpu type
+console.log("loadavg : ", os.loadavg()); // method returns an array containing the 1, 5, and 15 minute load averages
+console.log("platform : ", os.platform()); // paltform
+console.log("release : ", os.release()); // kernel release
+console.log("tmpdir : ", os.tmpdir()); // tmp directory
+console.log("total memory : ", os.totalmem()); // total Memory
+console.log("os type : ", os.type()); // os type
+console.log("up time : ", secondsToHms(os.uptime())); // os up time
+console.log("cpu : ", os.cpus()); //  information about each logical CPU core
+console.log("arch : ", os.arch()); // operating system CPU architecture
+console.log("network interfaces : ", os.networkInterfaces()); // network interfaces
 console.log("====================================");
+
+function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor((d % 3600) / 60);
+  var s = Math.floor((d % 3600) % 60);
+
+  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  return hDisplay + mDisplay + sDisplay;
+}
