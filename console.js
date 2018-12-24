d69f2fc134c31865e664865d5e818356bcbea301
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-24 13:34:25 +0100

    Errors and console

---
+++ console.js
@@ -0,0 +1,10 @@
+console.error("here is an error");
+const name = "jamal";
+console.warn(`My Name is: ${name}`);
+
+var count = 9;
+
+do {
+  count++;
+  console.log(`count ${count}`);
+} while (count < 19);
