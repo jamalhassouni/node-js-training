3da5a877e49a8f8207a5ce28149e7d89baf96c21
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-25 23:24:15 +0100

    Node js API Process

---
+++ process.js
@@ -0,0 +1,7 @@
+console.log(process.arch); // process arch
+console.log(process.pid); // process id
+console.log(process.platform); // platform
+console.log(process.version); // process version
+console.log(process.cwd()); // process  directroy path
+console.log(process.uptime());
+console.log(process.memoryUsage());
