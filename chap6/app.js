a410e3728e701076b63ae0b8af13642d49a98702
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-28 21:56:26 +0100

    Node js Introduction to Express

---
+++ chap6/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("this is the homepage");
+});
+
+app.get("/contact", function(req, res) {
+  res.send("this is the contact page");
+});
+app.listen(3000);
