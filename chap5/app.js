f31de0f0bb3de38db9d0653b51a781602a5323ae
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-28 18:33:52 +0100

    Node js Creating a Server

--- chap5/app.js
+++ chap5/app.js
@@ -1,5 +1,16 @@
-const stuff = require("./stuff");
+// const stuff = require("./stuff");
 
-console.log(stuff.counter(["shaun", "crystal", "ryu"]));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5));
+// console.log(stuff.counter(["shaun", "crystal", "ryu"]));
+// console.log(stuff.adder(5, 6));
+// console.log(stuff.adder(stuff.pi, 5));
+
+const http = require("http");
+
+var server = http.createServer((req, res) => {
+  console.log("request was made:" + req.url);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hey Jamal");
+});
+
+server.listen(3000, "localhost");
+console.log("yo dawgs, now listening to port 3000");
