71627bf82023d507a8c907466fa6cfb222d53aaf
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-28 22:17:01 +0100

    Node js Express Route Params

--- chap6/app.js
+++ chap6/app.js
@@ -8,4 +8,7 @@ app.get("/", function(req, res) {
 app.get("/contact", function(req, res) {
   res.send("this is the contact page");
 });
+app.get("/profile/:id", function(req, res) {
+  res.send("you requested to see a profile with the id of " + req.params.id);
+});
 app.listen(3000);
