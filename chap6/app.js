aa7cdb4302a6fbda1885d3d0b02f3cd39713c6f6
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-29 11:56:29 +0100

    Node js Template Engines Part 1

--- chap6/app.js
+++ chap6/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
 app.get("/", function(req, res) {
-  res.send("this is the homepage");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/contact", function(req, res) {
-  res.send("this is the contact page");
+  res.sendFile(__dirname + "/contact.html");
 });
-app.get("/profile/:id", function(req, res) {
-  res.send("you requested to see a profile with the id of " + req.params.id);
+app.get("/profile/:name", function(req, res) {
+  let data = { age: 23, job: "web developer" };
+  res.render("profile", { person: req.params.name, data: data });
 });
 app.listen(3000);
