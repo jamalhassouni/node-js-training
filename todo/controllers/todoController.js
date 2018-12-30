8096524646d36e0f54d45ef3d4fa381b81afbe1f
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-30 15:42:12 +0100

    Node js Making a To-do App (Part 4)

--- todo/controllers/todoController.js
+++ todo/controllers/todoController.js
@@ -1,7 +1,27 @@
+const bodyParser = require("body-parser");
+
+let data = [
+  { item: "get milk" },
+  { item: "walk dog" },
+  { item: "kick some coding ass" }
+];
+const urlencodeParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function(app) {
   app.get("/todo", function(req, res) {
-    res.render("todo");
+    res.render("todo", { todos: data });
   });
-  app.post("/todo", function(req, res) {});
-  app.delete("/todo", function(req, res) {});
+
+  app.post("/todo", urlencodeParser, function(req, res) {
+    data.push(req.body);
+    res.json(data);
+  });
+
+  app.delete("/todo/:item", function(req, res) {
+    data = data.filter(todo => {
+      // replace space with -  and filter data
+      return todo.item.replace(/ /g, "-") !== req.params.item;
+    });
+    res.json(data);
+  });
 };
