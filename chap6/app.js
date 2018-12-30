40e3fd57a304c91db8c95271e2922c76ed7ea638
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-30 13:13:27 +0100

    Node js Handling POST Requests

--- chap6/app.js
+++ chap6/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+var urlencodeParser = bodyParser.urlencoded({ extended: false });
+
+// template engine ejs
 app.set("view engine", "ejs");
+
 // Middleware
 app.use("/assets", express.static("assets"));
 
@@ -12,6 +17,12 @@ app.get("/", function(req, res) {
 app.get("/contact", function(req, res) {
   res.render("contact", { qs: req.query });
 });
+
+app.post("/contact", urlencodeParser, function(req, res) {
+  console.log(req.body);
+  res.render("contactSuccess", { data: req.body });
+});
+
 app.get("/profile/:name", function(req, res) {
   let data = {
     age: 23,
