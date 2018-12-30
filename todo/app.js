cb76b92f6a0a5a1cfb3ae7af9c5306c50a2eb24f
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-12-30 14:02:08 +0100

    Node js Making a To-do App (Part 2)

---
+++ todo/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const todoController = require("./controllers/todoController");
+const app = express();
+
+// Set up tmeplate engine
+
+app.set("view engine", "ejs");
+
+// static files
+app.use("/assets", express.static("./public"));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+
+app.listen(3000);
+
+console.log("You are listening to port 3000");
---
+++ todo/controllers/todoController.js
@@ -0,0 +1,5 @@
+module.exports = function(app) {
+  app.get("/todo", function(req, res) {});
+  app.post("/todo", function(req, res) {});
+  app.delete("/todo", function(req, res) {});
+};
